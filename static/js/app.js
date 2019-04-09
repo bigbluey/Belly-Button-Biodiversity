330e86f025a8050eca586d9373d91fa76b815398
bigbluey <joseph.yon@gmail.com>
2019-04-09 11:41:18 -0700

    Initial Commit

---
+++ static/js/app.js
@@ -0,0 +1,58 @@
+// Belly Button Biodiversity - Plotly.js
+
+function buildMetadata(sample) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+
+  // Use `d3.json` to fetch the metadata for a sample
+    // Use d3 to select the panel with id of `#sample-metadata`
+
+    // Use `.html("") to clear any existing metadata
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+}
+
+function buildCharts(sample) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+
+    // @TODO: Build a Bubble Chart using the sample data
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+}
+
+function init() {
+  // Grab a Reference to the Dropdown Select Element
+  var selector = d3.select("#selDataset");
+
+  // Use the List of Sample Names to Populate the Select Options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the First Sample from the List to Build Initial Plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // Fetch New Data Each Time a New Sample is Selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the Dashboard
+init();
